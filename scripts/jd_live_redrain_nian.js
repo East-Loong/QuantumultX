e76ef076d3a5913e29741c97fba002fccfea20e5
dongzi-L <62493180+dongzi-L@users.noreply.github.com>
2021-03-25 09:02:37 +0800

    Update jd_live_redrain_nian.js

--- scripts/jd_live_redrain_nian.js
+++ scripts/jd_live_redrain_nian.js
@@ -47,17 +47,17 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
 let ids = {
-  '0':'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '9':'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '11':'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '13':'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '15':'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '17':'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '19':'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '20': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '21': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '22': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
-  '23': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX'
+  '0':'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '9':'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '11':'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '13':'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '15':'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '17':'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '19':'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '20': 'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '21': 'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '22': 'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK',
+  '23': 'RRA2v2Lh5mtxYnC4j6VHrjoaDz7crQK'
 }
 !(async () => {
   if (!cookiesArr[0]) {
