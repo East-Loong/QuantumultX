4c9f056368f47c62327bcfde6a3193aeb64355a4
dongzi-L <62493180+dongzi-L@users.noreply.github.com>
2021-04-01 19:15:16 +0800

    Update CamScanner.js

--- scripts/CamScanner.js
+++ scripts/CamScanner.js
@@ -1,18 +1,27 @@
-/*
-CamScanner unlocks pro, Cloud scanning is not available.
-
-QuanX 1.0.0:  [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-
-Quan:  [REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
-
-Surge4.0: [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-
-QX & Q & Surge Mitm = ap*.intsig.net,
-*/
-
-let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
-$done({body: JSON.stringify(obj)});
+let body = JSON.parse($response.body)
+if (body.data.psnl_vip_property) {
+    body.data.psnl_vip_property = {"renew_method": "appstore",
+      "initial_tm": "1614867690",
+      "svip": 1,
+      "auto_renewal": true,
+      "ms_first_pay": 0,
+      "pending": 0,
+      "group2_paid": 0,
+      "inherited_flag": 0,
+      "nxt_renew_tm": "9915126887",
+      "level_info": {
+        "level": 1,
+        "days": 1,
+        "end_days": 30
+      },
+      "group1_paid": 1,
+      "ys_first_pay": 0,
+      "renew_type": "year",
+      "expiry": 9915130487,
+      "grade": 2,
+      "last_payment_method": "appstore",
+      "product_id": "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low"}
+    $done({body:JSON.stringify(body)})
+} else {
+    $done({})
+}
